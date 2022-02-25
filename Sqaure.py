90d487837c8febc856bb9d027f63f3203c6e074f
Rejoy Chakraborty <63641754+rejoyc-cs@users.noreply.github.com>
2022-02-25 18:32:47 +0530

    Update Sqaure.py

--- Sqaure.py
+++ Sqaure.py
@@ -3,19 +3,32 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
+def reshape(w,h):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 30.0, 0.0, 35.0, -1.0, 1.0)
+    
+def mouse(button,state,x,y):
+	x=0
+	y=0
+	state=0
+	
+	if button==GLUT_LEFT_BUTTON:
+		glutLeaveMainLoop()
+
+
 def square_draw():
     glClear(GL_COLOR_BUFFER_BIT)
-    glPointSize(5.0)
     
     glBegin(GL_POLYGON)
     glColor3f(1.0, 0.1, 0.1)
-    glVertex2f(0.1, 0.6)
+    glVertex3i(10,10,0)
     glColor3f(0.1, 0.1, 0.0)
-    glVertex2f(0.6, 0.6)
+    glVertex3i(10,30,0)
     glColor3f(0.1, 0.1, 1.0)
-    glVertex2f(0.6, 0.1)
+    glVertex3i(30,30,0)
     glColor3f(0.1, 0.3, 0.1)
-    glVertex2f(0.1, 0.1)
+    glVertex3i(30, 10,0)
     glEnd()
     
     glFlush()
@@ -23,10 +36,10 @@ def square_draw():
 def main():
 	glutInit()
 	glutInitDisplayMode(GLUT_RGB)
-	glutCreateWindow("Line")
-	glutInitWindowSize(500, 500)
-	glutInitWindowPosition(500, 500)
+	glutCreateWindow("Square")
 	glutDisplayFunc(square_draw)
+	glutReshapeFunc(reshape)
+	glutMouseFunc(mouse)
 	glutMainLoop()
 
 if __name__ == '__main__':
