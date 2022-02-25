463579a63ad3b42fe90a3c8b44a09034cb194c6f
Rejoy Chakraborty <63641754+rejoyc-cs@users.noreply.github.com>
2022-02-25 18:32:25 +0530

    Update Line Draw.py

--- Line Draw.py
+++ Line Draw.py
@@ -3,14 +3,27 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
+def reshape(w,h):
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 30.0, 0.0, 35.0, -1.0, 1.0)
+    
+def mouse(button,state,x,y):
+	x=0
+	y=0
+	state=0
+	
+	if button==GLUT_LEFT_BUTTON:
+		glutLeaveMainLoop()
+
 def line_draw():
     glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(0.0,1.0,1.0)
-    glPointSize(5.0)
     
     glBegin(GL_LINES)
-    glVertex2f(0, 0)
-    glVertex2f(-0.65, 0.25)
+    glColor3f(0.45,0.75,1.0)
+    glVertex3i(10, 10, 0)
+    glColor3f(0.8,1.0,0.5)
+    glVertex3i(80, 80, 0)
     glEnd()
     
     glFlush()
@@ -19,9 +32,9 @@ def main():
 	glutInit()
 	glutInitDisplayMode(GLUT_RGB)
 	glutCreateWindow("Line")
-	glutInitWindowSize(500, 500)
-	glutInitWindowPosition(50, 50)
 	glutDisplayFunc(line_draw)
+	glutReshapeFunc(reshape)
+	glutMouseFunc(mouse)
 	glutMainLoop()
 
 if __name__ == '__main__':
